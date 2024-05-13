a6e61c9c4a4dabe3dc813fa72440b02c3dc82efe
AyaAlaa <133635391+laylaaa1@users.noreply.github.com>
2024-05-13 23:44:16 +0300

    login implementation

--- lib/features/login/manager/login_cubit.dart
+++ lib/features/login/manager/login_cubit.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+import 'package:ecommerce/core/failure/server_failure.dart';
+import 'package:ecommerce/core/services/web_serivces.dart';
 import 'package:ecommerce/data/data_source/login/login_data_source.dart';
 import 'package:ecommerce/data/data_source/login/login_data_source_imp.dart';
 import 'package:ecommerce/data/repository_imp/login_repository_imp.dart';
@@ -19,17 +21,27 @@ class LoginCubit extends Cubit<LoginStates>{
   late LoginUseCase loginUseCase;
   late LoginRepository loginRepository;
   late LoginDataSource loginDataSource;
-  late Dio dio;
 
 
-  Future <Either<Failure,bool>> login(String email, String password) async{
-    dio=Dio();
-    loginDataSource=LoginDataSourceImpl(dio);
+  Future <bool> login(String email, String password) async{
+    WebServices webServices=WebServices();
+    loginDataSource=LoginDataSourceImpl(webServices.freeDio);
     loginRepository=LoginRepositoryImp(loginDataSource);
     loginUseCase=LoginUseCase(loginRepository);
+    emit(LoadingLogin());
+
+    final result=  await  loginUseCase.execute(email, password);
+    return  result.fold((fail) {
+       var error= fail as ServerFailure;
+       emit(ErrorLogin());
+       return  Future.value(false);
+     }, (data) {
+       emit(SuccessLogin());
+       return Future.value(true);
+     });
+}
 
 
-    return await  loginUseCase.execute(email, password);
 
 
-}}
+}
